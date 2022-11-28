25bac90054a2621d7db19563cc23561ad29eee8d
Hoàng Mạnh Hùng <manhhung2720@gmail.com>
2022-11-28 14:29:10 +0700

    Complete game with simple UI

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,37 +1,81 @@
  
 let choices = ['Rock', 'Paper', 'Scissors'];
 
+let resultEl = document.querySelector('.weapon');
+let ruleEl = document.querySelector('.rule');
+let playerChoiceEl = document.querySelector('.player-choice');
+let playerScoreEl = document.querySelector('#player-score');
+let computerChoiceEl = document.querySelector('.computer-choice');
+let computerScoreEl = document.querySelector('#computer-score');
+let rockChoosenEl = document.querySelector('#Rock');
+let paperChoosenEl = document.querySelector('#Paper');
+let scissorsChoosenEl = document.querySelector('#Scissors');
+
+let userChoice = "";
+let playerScore = 0;
+let computerScore = 0;
+
+
+rockChoosenEl.addEventListener("click", function() {
+    userChoice = "rock";
+    let computerChoice = getComputerChoice();
+    userChoice = normalizeInput(userChoice);
+    game(userChoice, computerChoice);
+
+})
+
+scissorsChoosenEl.addEventListener("click", function() {
+    userChoice = "scissors";
+    let computerChoice = getComputerChoice();
+    userChoice = normalizeInput(userChoice);
+    game(userChoice, computerChoice);
+})
+
+paperChoosenEl.addEventListener("click", function() {
+    userChoice = "paper";
+    let computerChoice = getComputerChoice();
+    userChoice = normalizeInput(userChoice);
+    game(userChoice, computerChoice);
+})
+
+
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*3)]; // will return 0-2
 }
 
-function playRound(playerChoice, computerChoice) {
+function result(playerChoice, computerChoice) {
     if(playerChoice === 'Paper') {
         switch (computerChoice) {
             case 'Rock':
-                return `You win! Paper beats Rock`;
+                playerScore++;
+                return ['You win!', 'Paper beats Rock'];
             case 'Paper':
-                return `Tie! Paper ties paper`;
+                return [`Tie!`,  `Paper ties paper`];
             case 'Scissors':
-                return `You lose! Scissors beats Paper`;
+                computerScore++;
+                return [`You lose!`, `Scissors beats Paper`];
         }
     } else if(playerChoice === 'Rock') {
         switch (computerChoice) {
             case 'Rock':
-                return `Tie! Rock ties Rock`;
+                return [`Tie!`, `Rock ties Rock`];
             case 'Paper':
-                return `You lose! Paper beats Rock`;
+                computerScore++;
+                return [`You lose!`, `Paper beats Rock`];
             case 'Scissors':
-                return `You win! Rock beats Scissors`;
+                playerScore++;
+                return [`You win!`, `Rock beats Scissors`];
         }
     } else if(playerChoice === 'Scissors'){
         switch (computerChoice) {
             case 'Rock':
-                return `You lose! Rock beats Scissors`;
+                computerScore++;
+                return [`You lose!`, `Rock beats Scissors`];
             case 'Paper':
-                return `You win! Scissors beats Paper`;
+                playerScore++;
+                return [`You win!`, `Scissors beats Paper`];
             case 'Scissors':
-                return `Tie! Scissors ties Scissors`;
+                return [`Tie!`, `Scissors ties Scissors`];
         }
     } else {
         return "You enter invalid input";
@@ -43,16 +87,54 @@ function normalizeInput(playerChoice) {
     return playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
 }
 
-function game() {
-    for(let i = 1; i <= 5; i++){
-        let playerChoice = prompt("What is your choice? Rock, Paper or Scissors");
-        let computerChoice = getComputerChoice();
-        playerChoice = normalizeInput(playerChoice);
-        console.log(playRound(playerChoice, computerChoice));
+function renderComputerChoice(choice) {
+    if(choice === 'Paper') {
+        computerChoiceEl.textContent = '🖐🏼';
+    } else if(choice === 'Rock') {
+        computerChoiceEl.textContent = '✊🏼';
+    } else {
+        computerChoiceEl.textContent = '✌🏼';
     }
 }
 
-game();
+function renderPlayerChoice(choice) {
+    if(choice === 'Paper') {
+        playerChoiceEl.textContent = '🖐🏼';
+    } else if(choice === 'Rock') {
+        playerChoiceEl.textContent = '✊🏼';
+    } else {
+        playerChoiceEl.textContent = '✌🏼';
+    }
+}
+
+
+function game(playerChoice, computerChoice) {
+    if(playerScore === 5 || computerScore === 5) {
+        reset();
+        return;
+    }
+    let arrayAnswer = result(playerChoice, computerChoice);
+    resultEl.textContent = arrayAnswer[0];
+    ruleEl.textContent = arrayAnswer[1];
+    renderPlayerChoice(playerChoice);
+    renderComputerChoice(computerChoice);
+    playerScoreEl.textContent = `Player: ${playerScore}`;
+    computerScoreEl.textContent = `Computer: ${computerScore}`;
+}
+
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    playerChoiceEl.textContent = '?';
+    computerChoiceEl.textContent = '?';
+    resultEl.textContent = 'Choose your weapon';
+    ruleEl.textContent = 'First to score 5 points wins the game';
+    playerScoreEl.textContent = `Player: ${playerScore}`;
+    computerScoreEl.textContent = `Computer: ${computerScore}`;
+}
+
+console.log(userChoice);
+// game();
 
 
 
